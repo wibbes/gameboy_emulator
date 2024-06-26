654ff80e9ac7c62bb38c71c0101fcc0ee2164311
hyprd <soulitch7@gmail.com>
2024-06-26 15:10:23 +1200

    Give cartridge data to MMU

--- src/mmu.h
+++ src/mmu.h
@@ -1,11 +1,14 @@
 #ifndef GAMEBOY_EMULATOR_SRC_MMU_H_
 #define GAMEBOY_EMULATOR_SRC_MMU_H_
 
-#include <vector>
 #include <cstdint>
+#include <vector>
 
 class MMU {
+public:
   std::vector<uint8_t> memory_;
+  MMU(std::vector<uint8_t> cart) : memory_(cart){};
+  ~MMU() = default;
 };
 
 #endif
