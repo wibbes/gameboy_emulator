c8e571e9a8cc851811b59b722a79d43c55bf4817
hyprd <soulitch7@gmail.com>
2024-09-27 12:15:04 +1200

    Add switch case for MMU

--- src/mmu.cc
+++ src/mmu.cc
@@ -1,10 +1,14 @@
 #include "mmu.h"
 
 void MMU::WriteMemory(uint16_t address, uint8_t value) {
-  if (address == 0xFF01) {
+  switch (address) {
+  case 0xFF01:
     std::cout << std::hex << value;
+    break;
+  default:
+    memory_[address] = value;
+    break;
   }
-  memory_[address] = value;
 }
 
 uint8_t MMU::ReadMemory(uint16_t address) {
