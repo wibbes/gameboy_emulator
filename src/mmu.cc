ada5e903ceec49881a7200156705333afeb28c2f
hyprd <soulitch7@gmail.com>
2024-09-27 12:45:26 +1200

    Move InterruptRegister function implementations to mmu.cc

--- src/mmu.cc
+++ src/mmu.cc
@@ -1,5 +1,24 @@
 #include "mmu.h"
 
+void InterruptRegister::SetState(uint8_t state) {
+  // ints are implicitly converted so this is generally type-safe to do
+  state_ = (state & 0b00011111);
+}
+
+void InterruptRegister::SetInterrupt(uint8_t interrupt) {
+  state_.set(interrupt);
+}
+
+uint8_t InterruptRegister::GetState() {
+  if (state_.none())
+    return 0;
+  return static_cast<uint8_t>(state_.to_ulong());
+}
+
+uint8_t InterruptRegister::GetInterrupt(uint8_t interrupt) {
+  return state_.test(interrupt);
+}
+
 void MMU::WriteMemory(uint16_t address, uint8_t value) {
   switch (address) {
   case 0xFF01:
