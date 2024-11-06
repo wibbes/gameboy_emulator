049addff86d3eec779dbabafad04eb63c6774848
hyprd <soulitch7@gmail.com>
2024-11-06 15:03:15 +1300

    Move some MMU.h members to private

--- src/mmu.h
+++ src/mmu.h
@@ -11,9 +11,8 @@
 
 class InterruptRegister {
 public:
-  InterruptRegister() : state_(0x0b11111) {};
+  InterruptRegister() : state_(0x0b00000) {};
   ~InterruptRegister() = default;
-  std::bitset<5> state_;
 
   void SetState(uint8_t state);
   void SetInterrupt(uint8_t interrupt);
@@ -22,19 +21,21 @@ public:
   uint8_t GetInterrupt(uint8_t interrupt);
 
 private:
+  std::bitset<5> state_;
 };
 
 class MMU {
 public:
-  std::vector<uint8_t> memory_;
   std::unique_ptr<InterruptRegister> ie_, if_;
   MMU(std::shared_ptr<Cartridge> cart)
       : memory_(cart->data), ie_(std::make_unique<InterruptRegister>()),
         if_(std::make_unique<InterruptRegister>()) {}
   ~MMU() = default;
-
+  
   void WriteMemory(uint16_t address, uint8_t value);
   uint8_t ReadMemory(uint16_t address);
+private:
+  std::vector<uint8_t> memory_;
 };
 
 #endif
