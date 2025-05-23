370801c7aad7e2f26660c323c00c9a8a30ed98ba
miste2 <soulitch7@gmail.com>
2025-05-23 17:14:17 +1200

    Fix memory_ being wrong size

--- src/mmu.h
+++ src/mmu.h
@@ -8,17 +8,21 @@
 #include <vector>
 
 #include "cartridge.h"
-#include "timer.h"
 #include "interruptregister.h"
+#include "timer.h"
 
 class MMU {
 public:
   std::unique_ptr<InterruptRegister> ie_, if_;
   MMU(std::shared_ptr<Cartridge> cart, std::shared_ptr<Timer> timer)
-      : memory_(cart->data), timer_(timer), ie_(std::make_unique<InterruptRegister>()),
+      : memory_(65535), timer_(timer),
+        ie_(std::make_unique<InterruptRegister>()),
         if_(std::make_unique<InterruptRegister>()) {
-          timer_->LinkIF(if_.get());
-        }
+    timer_->LinkIF(if_.get());
+    for(int k  = 0; k < cart->data.size(); ++k) {
+      memory_.at(k) = cart->data[k];
+    }
+  }
   ~MMU() = default;
 
   void WriteMemory(uint16_t address, uint8_t value);
@@ -27,6 +31,8 @@ public:
 private:
   std::vector<uint8_t> memory_;
   std::shared_ptr<Timer> timer_;
+
+  void PopulateMemory(std::vector<uint8_t> cart);
 };
 
 #endif
